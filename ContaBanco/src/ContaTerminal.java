d2842d56a4ee40da10398fce700c53a8b52a9065
DenilsonNovais <denilson_n.santos@hotmail.com>
2025-03-06 04:47:03 -0300

    feat: add bank account coding

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        scanner.nextLine();
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta:");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
